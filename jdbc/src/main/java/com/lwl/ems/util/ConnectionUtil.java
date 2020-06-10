86bbc4dd5c42b395d0cb7f93307ad51932172c6a
Lakshman <tanvi.avula@outlook.com>
2020-06-10 08:39:31 +0530

    Added jdbc batch example

--- jdbc/src/main/java/com/lwl/ems/util/ConnectionUtil.java
+++ jdbc/src/main/java/com/lwl/ems/util/ConnectionUtil.java
@@ -1,110 +1,109 @@
 package com.lwl.ems.util;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public enum ConnectionUtil {
 	util;
-	
-		private static Properties properties;
-		static {
-			properties = new Properties();
-			try {
-				//properties.load(Thread.currentThread().getClass().getResourceAsStream("/db.properties"));
-				properties.load(ConnectionUtil.class.getResourceAsStream("/db.properties"));
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+
+	HikariDataSource ds;
+
+	{
+		HikariConfig config = new HikariConfig(ConnectionUtil.class.getResource("/db.properties").getPath());
+		ds = new HikariDataSource(config);
+
+	}
+
+	public Connection getConnection() {
+		Connection con = null;
+		try {
+			return ds.getConnection();
+		} catch (Exception e) {
+			System.out.println("While connecting with db:" + e);
 		}
-	
-		public Connection getConnection() {
-			Connection con = null;
-			try {
-			
-				con = DriverManager.getConnection(properties.getProperty("url"),properties);
-			}catch (Exception e) {
-				System.out.println("While connecting with db:"+e);
-			}
-			return con;
-			
+		return con;
+
+	}
+
+	public void close(ResultSet rs, Statement st, Connection con) {
+		try {
+			if (rs != null)
+				rs.close();
+			if (st != null)
+				st.close();
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			System.out.println("While closing resources :" + e);
 		}
-		public void close(ResultSet rs, Statement st, Connection con) {
-			try {
-				if(rs != null) 
-					rs.close();
-				if(st!=null)
-					st.close();
-				if(con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.out.println("While closing resources :"+e);
-			}
+	}
+
+	public void close(Statement st, Connection con) {
+		try {
+
+			if (st != null)
+				st.close();
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			System.out.println("While closing resources :" + e);
 		}
-		public void close(Statement st, Connection con) {
-			try {
-				
-				if(st!=null)
-					st.close();
-				if(con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.out.println("While closing resources :"+e);
-			}
+	}
+
+	public void close(ResultSet rs, PreparedStatement pst, Connection con) {
+		try {
+			if (rs != null)
+				rs.close();
+			if (pst != null)
+				pst.close();
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			System.out.println("While closing resources :" + e);
 		}
-		public void close(ResultSet rs, PreparedStatement pst, Connection con) {
-			try {
-				if(rs != null) 
-					rs.close();
-				if(pst!=null)
-					pst.close();
-				if(con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.out.println("While closing resources :"+e);
-			}
+	}
+
+	public void close(PreparedStatement pst, Connection con) {
+		try {
+
+			if (pst != null)
+				pst.close();
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			System.out.println("While closing resources :" + e);
 		}
-		
-		public void close(PreparedStatement pst, Connection con) {
-			try {
-				
-				if(pst!=null)
-					pst.close();
-				if(con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.out.println("While closing resources :"+e);
-			}
+	}
+
+	public void close(ResultSet rs, CallableStatement cst, Connection con) {
+		try {
+			if (rs != null)
+				rs.close();
+			if (cst != null)
+				cst.close();
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			System.out.println("While closing resources :" + e);
 		}
-		public void close(ResultSet rs, CallableStatement cst, Connection con) {
-			try {
-				if(rs != null) 
-					rs.close();
-				if(cst!=null)
-					cst.close();
-				if(con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.out.println("While closing resources :"+e);
-			}
+	}
+
+	public void close(CallableStatement cst, Connection con) {
+		try {
+
+			if (cst != null)
+				cst.close();
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			System.out.println("While closing resources :" + e);
 		}
-		public void close(CallableStatement cst, Connection con) {
-			try {
-			
-				if(cst!=null)
-					cst.close();
-				if(con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.out.println("While closing resources :"+e);
-			}
-		}
+	}
 }
